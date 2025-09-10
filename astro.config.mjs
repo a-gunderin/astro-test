ab1c6a7a3b7d195010a606b4dd9fc031cf3771da
Andrei Gunderin <ga.webnotes@gmail.com>
2025-09-10 15:43:39 +0300

    chore: step 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   site: 'https://stormy-comfort-astro.surge.sh/',
+  integrations: [preact()],
 });
